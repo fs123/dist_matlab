53bc4290863fb2bd35925df7b7455a23f1eae667
Flavio <fs@sadeghi.ch>
2015-06-30 13:44:17 +0200

    fixed mmsInf_p0(...)

--- mmsInf_p0.m
+++ mmsInf_p0.m
@@ -9,13 +9,13 @@ function [ endValue ] = mmsInf_p0( h, u, s )
 
     endValue = 0;
     for i = 0:(s-1)
-        endValue = ((1 / factorial(i)) * (h/u)^i);
+        endValue = endValue + ((1 / factorial(i)) * (h/u)^i);
     end
     
     x1 = (1 / factorial(s));
     x2 = (h/u)^s;
     x3 = (s/(s-(h/u)));
-    value2 = x1 * x2 * x3
+    value2 = x1 * x2 * x3;
     endValue = endValue + value2 ;
     endValue = endValue^(-1);
     
